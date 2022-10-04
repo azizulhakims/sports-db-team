4d645ed959bd2b81c2146693dcb149ffb90cea1a
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-04 21:44:32 +0600

    add localstorage

--- src/components/SinglePlayer/SinglePlayer.js
+++ src/components/SinglePlayer/SinglePlayer.js
@@ -3,7 +3,7 @@ import './SinglePlayer.css'
 
 const SinglePlayer = ({player, cart, setCart}) => {
     // console.log(player);
-    const {strNationality, strPlayer, idPlayer, strDescriptionEN, strCutout} = player;
+    const { strPlayer, idPlayer, strDescriptionEN, strCutout} = player;
 
 
 
@@ -15,23 +15,6 @@ const SinglePlayer = ({player, cart, setCart}) => {
             strDescriptionEN,
             price: 115,
         };
-
-        const handleBookmark = () =>{
-            const info = {
-                idPlayer,
-                strPlayer,
-                strCutout,
-                strDescriptionEN,
-                price: 115,
-                bookmark:'true',
-            };
-            console.log(info);
-        };
-
-        
-
-
-
         if(cart){
             const newPlayer = [...cart, info];
             setCart(newPlayer);
@@ -41,16 +24,47 @@ const SinglePlayer = ({player, cart, setCart}) => {
 
     };
 
+    const handleSetBookmark = () =>{
+        const info = {
+            idPlayer,
+            strPlayer,
+            strCutout,
+            strDescriptionEN,
+            price: 115,
+            bookmark:'true',
+        };
+        // console.log(info);
+        const prevBookmark = localStorage.getItem('bookmark');
+        const oldBookMark = JSON.parse(prevBookmark);
+        if(oldBookMark){
+            const isExit = oldBookMark.find(p=>p.idPlayer===idPlayer);
+            console.log(isExit)
+
+            if(isExit){
+                alert('Already bookmarkded');
+                return;
+            } else{
+                localStorage.setItem('bookmark', JSON.stringify([...oldBookMark, info]));
+            }
+
+            localStorage.setItem('bookmark', JSON.stringify([...oldBookMark, info]));
+        }else{
+            localStorage.setItem('bookmark', JSON.stringify([info]));
+             console.log("nai");
+        }
+        // console.log(JSON.parse(prevBookmark));
+    };
+
     // console.log(cart);
 
     return (
-        <div className='card' data-aos='fade-up'>
+        <div className='card' data-aos ='zoom-in'>
             <img className='player-img' src={strCutout} alt="" />
             <h6> {strPlayer}</h6>
-            <p>{strDescriptionEN? strDescriptionEN?.slice(0,20):"lorem hello 123"}</p>
+            {/* <p>{strDescriptionEN? strDescriptionEN?.slice(0,20):"lorem hello 123"}</p> */}
             <button className='card-btn'>Details</button>
             <button onClick={handleAddToCart} className='card-btn'>Add to Cart</button>
-            <button onClick={handleBookmark} className='card-btn'>Bookmark</button>
+            <button onClick={handleSetBookmark} className ='card-btn'>Bookmark</button>
         </div>
     );
 };
